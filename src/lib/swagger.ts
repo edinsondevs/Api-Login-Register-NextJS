ca60dba3ce8cc4b364e5f9f0ba1a0b3bf0e94541
Edinson Madrid <edinsonmadrid@gmail.com>
2025-07-08 00:03:41 -0300

    refactor(swagger): mejorar la estructura de rutas para la generación de especificaciones API

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -2,33 +2,33 @@ import swaggerJsdoc from 'swagger-jsdoc';
 import path from 'path';
 
 const options: swaggerJsdoc.Options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API de Autenticación con Next.js',
-      version: '1.0.0',
-      description:
-        'Una API simple para la autenticación de usuarios construida con Next.js, MongoDB y TypeScript.',
-    },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
-  },
-  apis: [
-    path.join(process.cwd(), 'src', 'pages', 'api', '**', '*.ts'),
-    path.join(process.cwd(), 'src', 'schemas', '*.ts'),
-  ],
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "API de Autenticación con Next.js",
+			version: "1.0.0",
+			description:
+				"Una API simple para la autenticación de usuarios construida con Next.js, MongoDB y TypeScript.",
+		},
+		components: {
+			securitySchemes: {
+				bearerAuth: {
+					type: "http",
+					scheme: "bearer",
+					bearerFormat: "JWT",
+				},
+			},
+		},
+		security: [
+			{
+				bearerAuth: [],
+			},
+		],
+	},
+	apis: [
+		path.join(process.cwd(), "src/pages/api/**/*.ts"),
+		path.join(process.cwd(), "src/schemas/**/*.ts"), // Si tienes esquemas
+	],
 };
 
 export const swaggerSpec = swaggerJsdoc(options);
