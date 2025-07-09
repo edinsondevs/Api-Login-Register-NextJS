b6dd6238c1f1f72794e0058ef025e40fffa9da15
Edinson Madrid <edinsonmadrid@gmail.com>
2025-07-08 21:36:07 -0300

    refactor: reemplazar la implementación de Swagger con next-swagger-doc en doc.ts

--- src/pages/api/doc.ts
+++ src/pages/api/doc.ts
@@ -1,8 +1,30 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { swaggerSpec } from '@/src/lib/swagger';
+import { withSwagger } from 'next-swagger-doc';
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(swaggerSpec);
-};
+const swaggerHandler = withSwagger({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Autenticación con Next.js',
+      version: '2.0.0',
+      description:
+        'Una API simple para la autenticación de usuarios construida con Next.js, MongoDB y TypeScript.',
+    },
+		components: {
+			securitySchemes: {
+				bearerAuth: {
+					type: "http",
+					scheme: "bearer",
+					bearerFormat: "JWT",
+				},
+			},
+		},
+		security: [
+			{
+				bearerAuth: [],
+			},
+		],
+  },
+  apiFolder: 'src/pages/api',
+});
 
-export default handler;
+export default swaggerHandler();
