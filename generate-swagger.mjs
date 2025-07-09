e3c59947cb1347769d74869a839c5007dcddb96e
Edinson Madrid <edinsonmadrid@gmail.com>
2025-07-08 21:43:49 -0300

    feat: implementar generación de documentación OpenAPI y eliminar archivo obsoleto

---
+++ generate-swagger.mjs
@@ -0,0 +1,31 @@
+import { createSwaggerSpec } from 'next-swagger-doc';
+import fs from 'fs';
+
+const spec = createSwaggerSpec({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Autenticación con Next.js',
+      version: '1.2.1',
+      description:
+        'Una API simple para la autenticación de usuarios construida con Next.js, MongoDB y TypeScript.',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apiFolder: 'src/pages/api',
+});
+
+fs.writeFileSync('public/openapi.json', JSON.stringify(spec, null, 2));
